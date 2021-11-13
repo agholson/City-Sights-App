9b019d17af34746951457095e3754f5a313deb54
agholson <87455899+agholson@users.noreply.github.com>
2021-11-13 11:20:07 -0500

    - Added annotations to each business location   - Displays business name, when tapped   - Also, displays a business information icon, we can press to show more information - Created a businessReuseIdentifier that we will use to re-use objects in memory

--- City Sights App/Helpers/Constants.swift
+++ City Sights App/Helpers/Constants.swift
@@ -12,7 +12,12 @@ import Foundation
  */
 struct Constants {
     
+    // Static variables allow us to reference these properties without initializing an object of the Constants struct
     static var apiUrl = "https://api.yelp.com/v3/businesses/search"
     static var restaurantsKey = "restaurants"
     static var sightsKey = "arts"
+    
+    // Used in our BusinessMap view
+    static var annotationReuseId = "business"
+    
 }
--- City Sights App/Views/Home/Map/BusinessMap.swift
+++ City Sights App/Views/Home/Map/BusinessMap.swift
@@ -15,6 +15,9 @@ struct BusinessMap: UIViewRepresentable {
     
     @EnvironmentObject var model: ContentModel
     
+    /*
+     Computed property displays the business name, and an information icon when called
+     */
     var locations: [MKPointAnnotation] {
         
         // Create empty array of MKAnnotations
@@ -56,6 +59,10 @@ struct BusinessMap: UIViewRepresentable {
         
         let mapView = MKMapView()
         
+        // Allows us to display extra information with a click on the pins
+        // The system will call below makeCoordinator, then if it already exists, it will use the existing one
+        mapView.delegate = context.coordinator
+        
         // Display user on map
         mapView.showsUserLocation = true
         
@@ -88,6 +95,62 @@ struct BusinessMap: UIViewRepresentable {
     static func dismantleUIView(_ uiView: MKMapView, coordinator: ()) {
         // Remove the map annotations
         uiView.removeAnnotations(uiView.annotations)
+    }
+    
+    // MARK: Coordinator Class
+    
+    /*
+     This makeCoordinator is an optional method with the MKMapView
+     */
+    func makeCoordinator() -> Coordinator {
+        return Coordinator()
+    }
+    
+    /*
+     Coordinator class allows us to implement custom MKMapViewDelegate class, which handles UI events on the screen
+     
+     Coordinator only available inside of this struct
+     
+     Inherits from NSObject, because MKMapViewDelegate needs to be subclass of the NSObject class.
+     */
+    class Coordinator: NSObject, MKMapViewDelegate {
+     
+        // Creates an annotation with the business name for every single annotation on the map
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            
+            // If the annotation represent's the user's blue dot, then return nil.
+            // Without this, the user's locationd does not display on the map
+            if annotation is MKUserLocation { // sees if the annotation is the type MKUserLocation versus MKAnnotation
+                return nil
+            }
+            
+            // Check if we can re-use an annotation view, versus create one first
+            // Returns optional annotationView we set below, if it did not exist
+            var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: Constants.annotationReuseId)
+            
+            // If the above annotationView is nil, then create a new one
+            if annotationView == nil {
+                // Create an annotation view
+                // Reuse identifier allows us to use one of the current annotations that may not be on the screen, versus showing a new one,
+                // creating a new object in memory
+                annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: Constants.annotationReuseId)
+                
+                // Allows us to display extra information in a callout bubble
+                annotationView!.canShowCallout = true
+                
+                // Creates a small I information icon with the .detailDisclosur, the user can click to show details of the business
+                annotationView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+            }
+            else {
+                // Means we have a reusable annotation view, so we assign the old annotation to this one
+                annotationView!.annotation = annotation
+            }
+            
+            // Return it
+            return annotationView
+            
+        }
+        
     }
     
 }
